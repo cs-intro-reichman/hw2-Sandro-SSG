5b2c9a6402aba85d73a78dfc0e0a07af30cebe06
unknown <Sandro-SSG>
2025-11-13 13:37:15 +0200

    Assignment completed

--- CalcPi.java
+++ CalcPi.java
@@ -3,16 +3,16 @@ public class CalcPi {
 	public static void main(String [] args) { 
 		int b = Integer.parseInt(args[0]);
 	    double a = Math.PI;
-		int c = (b*2) - 3;
+		int c = (b*2) - 1;
 		int j = (b*2) - 1;
 		double sum = 0;
 		double dif = 0;
-		for( int i = 1; i < b; i += 4){
-		double d = (double)1 / i;
+		for( int i = 1; i <= c; i+= 4){
+		double d = (double)1 /(double) i;
 		sum += d;
 		}
-		for( int f = 3; f < b; f += 4){
-		double d = (double)1 / f;
+		for( int f = 3; f <= j; f += 4){
+		double d = (double)1 / (double)f;
 		dif += d;
 		}
 		System.out.println( "pi according to Java: " + a);
--- Collatz.java
+++ Collatz.java
@@ -9,40 +9,40 @@ public class Collatz {
 		int v = 1;
 		int z = 0;
 		if(aa){
-		if(num == 1){
-			int c = (v * 3) + 1;
-			int d = c /2;
-			int e = d /2;
-			int x = 4;
-			System.out.println(v + " " + c + " " + d + " " + e + " (" + x + ")");
-		}else{
-			int c = (v * 3) + 1;
-			int d = c /2;
-			int e = d /2;
-			int x = 4;
-			System.out.println(v + " " + c + " " + d + " " + e + " (" + x + ")");
-			for( int i = 2; i <= num; i++){
+			if(num == 1){
+				int c = (v * 3) + 1;
+				int d = c /2;
+				int e = d /2;
+				int x = 4;
+				System.out.println(v + " " + c + " " + d + " " + e + " (" + x + ")");
+			}else{
+				int c = (v * 3) + 1;
+				int d = c /2;
+				int e = d /2;
+				int x = 4;
+				System.out.println(v + " " + c + " " + d + " " + e + " (" + x + ")");
+				for( int i = 2; i <= num; i++){
 				System.out.print(i + " ");
 				a = i;
 				z = 1;
-				while ( a != 1 ) {
-					if(a % 2 != 0){
-						a *= 3;
-						a++;
-						z++;
+					while ( a != 1 ) {
+						if(a % 2 != 0){
+							a *= 3;
+							a++;
+							z++;
 							System.out.print(a + " ");
-					}else{
-						a /= 2;
-						z++;
+						}else{
+							a /= 2;
+							z++;
 							System.out.print(a + " ");
+						}
+					}
+				System.out.print( "(" + z + ")");
+				System.out.println();
 				}
 			}
-			System.out.print( "(" + z + ")");
-			System.out.println();
-		}
-		}
 		}else if( bb ){
-				System.out.println("Every one of the first " + num + " hailstone sequences reached 1.");
+			System.out.println("Every one of the first " + num + " hailstone sequences reached 1.");
 		}else{
 			System.out.println("Incorrect value of letter");
 		}
