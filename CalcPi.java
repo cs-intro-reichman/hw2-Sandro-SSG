5152dd7c7b1020498a6e6ac8790fa12230a54b2f
unknown <Sandro-SSG>
2025-11-13 08:59:39 +0200

    Assignment completed

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,22 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+		int b = Integer.parseInt(args[0]);
+	    double a = Math.PI;
+		int c = (b*2) - 3;
+		int j = (b*2) - 1;
+		double sum = 0;
+		double dif = 0;
+		for( int i = 1; i < b; i += 4){
+		double d = (double)1 / i;
+		sum += d;
+		}
+		for( int f = 3; f < b; f += 4){
+		double d = (double)1 / f;
+		dif += d;
+		}
+		System.out.println( "pi according to Java: " + a);
+		System.out.println( "pi, approximated: " + (sum - dif) * 4);
+		
+}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,24 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd = Integer.parseInt(args[1]);
+        int totalMinutes = (hours * 60) + minutes + minutesToAdd ;  
+        int hours1 = totalMinutes / 60; 
+        int hours2 = hours1 % 24;
+        int minutes1 = totalMinutes - (hours1 * 60); 
+        if(minutes1 < 10){
+            if(hours2 < 10){
+                System.out.println("0" + hours2 + ":0" + minutes1);
+            }else{
+            System.out.println(hours2 + ":0" + minutes1);
+            }
+        }else{
+            if(hours2 < 10){
+                System.out.println("0" + hours2 + ":" + minutes1);
+            }else{
+        System.out.println(hours2 + ":" + minutes1);
+        }
+        }
     }
 }
