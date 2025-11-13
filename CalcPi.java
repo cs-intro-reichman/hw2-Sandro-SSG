5781cb667baf9f1bc8347c9c3b9d38ae00db34be
unknown <Sandro-SSG>
2025-11-13 15:01:08 +0200

    Assignment completed

--- CalcPi.java
+++ CalcPi.java
@@ -1,32 +1,21 @@
-// Computes an approximation of PI.
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+// Computes an approximation of PI
 
 public class CalcPi {
 	public static void main(String [] args) { 
-		int b = Integer.parseInt(args[0]);
+		int j = Integer.parseInt(args[0]);
 	    double a = Math.PI;
-		int c = (b*2) - 1;
-		int j = (b*2) - 1;
-		double sum = 0;
+		int b = 0;
+		int v = 1;
+		double s = 1;
 		double dif = 0;
-		for( int i = 1; i <= c; i+= 4){
-		double d = (double)1 /(double) i;
-		sum += d;
-		}
-		for( int f = 3; f <= j; f += 4){
-		double d = (double)1 / (double)f;
-		dif += d;
-		}
-		double approx = (sum - dif) * 4;
-        System.out.printf("pi according to Java: %.15f%n", a);
-        BigDecimal formatted = new BigDecimal(approx)
-                .setScale(16, RoundingMode.DOWN);
-        String text = formatted.stripTrailingZeros().toPlainString();
-        if (text.endsWith(".")) {
-            text += "0";
-        }
-        System.out.println("pi, approximated:     " + text);
-    }
+	while ( b < j){
+		double x = 1 / s;
+		dif = dif + (x * v);
+		v = v * (-1);
+		s = s + 2;
+		b = b + 1;
+	}
+	System.out.println("pi according to Java: " + a);
+	System.out.println("pi, approximated:     " + (double)(dif * 4));
+	}
 }
